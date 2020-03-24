0996308dcd9c82b8e6d9cda00fc5bc04506cb600
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-03-23 19:55:39 -0600

    test

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ massive({
 if(process.env.NODE_ENV==='production'){
   app.use(express.static('build'))
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build','index.html'))
+    res.sendFile(path.join(__dirname,'build','index.html'))
   })
 }
 
