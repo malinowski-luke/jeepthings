fa246b3c6352aba782f5966d35d488fd2c98766f
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-03-30 23:13:17 -0400

    css fixed

--- src/components/DisplayPost/DisplayPost.js
+++ src/components/DisplayPost/DisplayPost.js
@@ -5,16 +5,16 @@ import './DisplayPost.scss'
 function DisplayPost(props) {
   return (
     <div className='DisplayPost'>
-      <div className='display-post-header'>
-        <h1 className='display-post-text'>{props.title}</h1>
-      </div>
       <div className='display-post-content'>
         <img
           src={props.img || defaultImg}
           alt='item img'
           className='display-post-img'
         />
-        <h1 className='display-post-text'>${props.price}</h1>
+        <div>
+          <h1 className='display-post-text'>{props.title}</h1>
+          <h1 className='display-post-text price'>${props.price}</h1>
+        </div>
       </div>
     </div>
   )
